44d4a294b09251fd38fad21311dead7503f21077
Mauro Spinello <mauro4889@gmail.com>
2022-06-05 22:54:08 -0300

    Tercer commit

--- index.js
+++ index.js
@@ -1,67 +1,62 @@
 const url = `https://pokeapi.co/api/v2/pokemon/bulbasaur`;
-const url2 = `https://pokeapi.co/api/v2/pokemon?limit=5&offset=0`;
+const url2 = `https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`;
 const templateCard = document.querySelector('#templateCard').content;
 const fragment = document.createDocumentFragment();
-const card = document.querySelector('.container__cards')
-const card5 = document.querySelector('.container__Cards5')
+const containerCard = document.querySelector('.container__cards');
+const card = document.querySelector('.card');
 const btnnext = document.getElementById('btnNext');
 const btnprev = document.getElementById('btnPrev');
 
 let next, prev;
 
-const mostrarPokemon = async (datosApi)=>{
+const mostrarapi = async(url) => {
+        const api = await fetch(url);
+        return await api.json();
+    }
+    /* RENDERIZAR POKEMON */
+const mostrarPokemon = async(datosApi) => {
+    /* SE CREA UN ARRAY CON LA URL DE LOS POKEMON OBTENIDA DEL FETCH */
+    const urlApi = datosApi.results.map((pokemon) => pokemon.url)
+    next = datosApi.next
+    prev = datosApi.prev
+    console.log(urlApi)
 
-  const urlApi = datosApi.results.map((pokemon)=> pokemon.url)
-  next = datosApi.next
-  prev = datosApi.prev
+    /* SE CREA UN ARRAY CON LA INFORMACION DE LOS POKEMON OBTENIDA DE SU URL*/
+    const datosPokemon = await Promise.all(
+        urlApi.map(async(url) => {
+            const next10 = await fetch(url);
+            return await next10.json();
+        })
+    )
 
-  const datosPokemon = await Promise.all(
-    urlApi.map(async (url)=>{
-      const next5 = await fetch(url);
-      return await next5.json();
-    })
-  )
-
+    /* SE RECORRE EL ARRAY Y SE RENDERIZAN LOS POKEMON EN EL FRAGMENT UNA VEZ SE  CARGARON TODOS SE CLONA EL TEMPLATE Y SE LO AGREGA AL .CONTAINER__CARD*/
+    datosPokemon.forEach((pokemon) => {
+            templateCard.querySelector('h2').textContent = pokemon.name;
+            templateCard.querySelector('h3').textContent = pokemon.id;
+            templateCard.querySelector('img').src = pokemon.sprites.other.dream_world.front_default || json.sprites.front_default;
+            const clone = document.importNode(templateCard, true);
+            fragment.appendChild(clone);
+            containerCard.appendChild(fragment);
+        })
+        /* FIN RENDERIZAR POKEMON */
+    console.log(datosPokemon)
 }
 
-const listaPokemon = async (url) => {
-  try {
-    const res = await fetch(url);
-    const data = await res.json();
-    mostrarPokemon(data);
-  } catch (error) {
-    consoñe.log('Error al obtener la lista de pokemons');
-  }
+
+/* LLAMADA A LA API*/
+const listaPokemon = async(url) => {
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        mostrarPokemon(data);
+    } catch (error) {
+        consoñe.log('Error al obtener la lista de pokemons');
+    }
 };
+/* FIN LLAMADA  A LA API*/
 
 
-
-document.addEventListener('DOMContentLoaded',()=>{
-  listaPokemon(url2)
+document.addEventListener('DOMContentLoaded', () => {
+    listaPokemon(url2)
+    mostrarapi(url2)
 })
-
-
-
-
-
-
-// fetch(url2)
-//   .then(obj =>{
-//     data = obj.json()
-//     console.log(data)
-//     return data
-//   })
-//   .then(json =>{
-//     const clone = templateCard.cloneNode(true);
-//     clone.querySelector('h2').textContent = json.name;
-//     clone.querySelector('h3').textContent = json.id;
-//     clone.querySelector('img').src = json.sprites.other.dream_world.front_default || json.sprites.front_default;
-//     fragment.appendChild(clone);
-//     card.appendChild(fragment);
-//   })
-//   .catch((err) =>{
-//     console.log(err)
-//   });
-
-
-

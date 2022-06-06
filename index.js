89697f3c9554e3297aa48dc27493c5083dfd7f81
mauro4889 <mauro4889@gmail.com>
2022-06-06 12:49:49 -0300

    Se agrego estilos y evento click

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const url = `https://pokeapi.co/api/v2/pokemon/bulbasaur`;
-const url2 = `https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`;
+const url = `https://pokeapi.co/api/v2/pokemon?limit=6&offset=0`;
 const templateCard = document.querySelector('#templateCard').content;
 const fragment = document.createDocumentFragment();
 const containerCard = document.querySelector('.container__cards');
-const card = document.querySelector('.card');
+const card = document.getElementById('card')
 const btnnext = document.getElementById('btnNext');
 const btnprev = document.getElementById('btnPrev');
 
@@ -17,9 +16,8 @@ const mostrarapi = async(url) => {
 const mostrarPokemon = async(datosApi) => {
     /* SE CREA UN ARRAY CON LA URL DE LOS POKEMON OBTENIDA DEL FETCH */
     const urlApi = datosApi.results.map((pokemon) => pokemon.url)
-    next = datosApi.next
-    prev = datosApi.prev
-    console.log(urlApi)
+    next = datosApi.next;
+    prev = datosApi.previous;
 
     /* SE CREA UN ARRAY CON LA INFORMACION DE LOS POKEMON OBTENIDA DE SU URL*/
     const datosPokemon = await Promise.all(
@@ -32,14 +30,29 @@ const mostrarPokemon = async(datosApi) => {
     /* SE RECORRE EL ARRAY Y SE RENDERIZAN LOS POKEMON EN EL FRAGMENT UNA VEZ SE  CARGARON TODOS SE CLONA EL TEMPLATE Y SE LO AGREGA AL .CONTAINER__CARD*/
     datosPokemon.forEach((pokemon) => {
             templateCard.querySelector('h2').textContent = pokemon.name;
-            templateCard.querySelector('h3').textContent = pokemon.id;
+            templateCard.querySelector('h3').textContent = `# ${pokemon.id}`;
             templateCard.querySelector('img').src = pokemon.sprites.other.dream_world.front_default || json.sprites.front_default;
             const clone = document.importNode(templateCard, true);
             fragment.appendChild(clone);
             containerCard.appendChild(fragment);
         })
         /* FIN RENDERIZAR POKEMON */
-    console.log(datosPokemon)
+    if (next !== ''){
+        btnnext.disabled = false;
+        btnnext.classList.remove('disabled')
+    } else{
+        btnnext.disabled = true;
+        btnnext.classList.add('disabled');
+        console.log(next)
+    }
+
+    if (prev){
+        btnprev.disabled = false;
+        btnprev.classList.remove('disabled')
+    } else{
+        btnprev.disabled = true;
+        btnprev.classList.add('disabled')
+    }
 }
 
 
@@ -57,6 +70,27 @@ const listaPokemon = async(url) => {
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    listaPokemon(url2)
-    mostrarapi(url2)
+    listaPokemon(url)
+    mostrarapi(url)
 })
+
+btnnext.addEventListener('click', (e) =>{
+    while (containerCard.firstChild){
+        containerCard.removeChild(containerCard.firstChild);
+    }
+    listaPokemon(next);
+    mostrarapi(next)
+})
+btnprev.addEventListener('click', (e) =>{
+    while (containerCard.firstChild){
+        containerCard.removeChild(containerCard.firstChild);
+    }
+    listaPokemon(prev);
+    mostrarapi(prev);
+})
+
+containerCard.forEach(el =>{
+    card.addEventListener('click', ()=>{
+
+    })
+} )
